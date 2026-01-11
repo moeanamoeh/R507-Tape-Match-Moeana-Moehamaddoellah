04e233d73bf80b13459be52e583379e8debc23b3
Moeana <moeanamoeh@outlook.fr>
2026-01-12 04:54:33 +1100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/R507-Tap-Match/', 
-  build: {
-    outDir: 'dist'
-  }
+  base: '/R507-Tape-Match-Moeana-Moehamaddoellah/',
 });
